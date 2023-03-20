5d8203658434f03cb35102b8554d0b9b3dbbc2d9
dorrajmachai <dwimble22@protonmail.com>
2023-03-20 05:12:01 -0600

    fixed error in text returned from api and started styles from scratch

--- src/index.js
+++ src/index.js
@@ -18,6 +18,9 @@ const wordAndEt = {
   def: ''
 }
 
+const cleanOutput = new RegExp("{it}", "g")
+const cleanOutputTwo = new RegExp("{/\it}", "g")
+
 const getWord = async(input) => {
   input = word.value;
   const url = `https://www.dictionaryapi.com/api/v3/references/collegiate/json/${input}?key=${api_key}`;
@@ -30,15 +33,29 @@ const getWord = async(input) => {
     
     console.log(data)
     
+    // the following code is horrible and i apologize to future us for having to maintain this. please forgive me for assaulting your eyeballs.
     wordAndEt.word = data[0].hwi.hw;
     wordAndEt.et = data[0]?.et[0][1];
     wordAndEt.pronunciation = data[0]?.hwi?.prs[0]?.mw;
     wordAndEt.short = data[0]?.shortdef[0];
     wordAndEt.def = data[0]?.shortdef[1];
 
+    wordAndEt.word = wordAndEt.word.replaceAll(cleanOutput, '<em>');
+    wordAndEt.et = wordAndEt.et.replaceAll(cleanOutput, '<em>');
+    wordAndEt.pronunciation = wordAndEt.pronunciation.replaceAll(cleanOutput, '<em>');
+    wordAndEt.short = wordAndEt.short.replaceAll(cleanOutput, '<em>');
+    wordAndEt.def = wordAndEt.def.replaceAll(cleanOutput, '<em>');
+
+    wordAndEt.word = wordAndEt.word.replaceAll(cleanOutputTwo, '</em>');
+    wordAndEt.et = wordAndEt.et.replaceAll(cleanOutputTwo, '</em>');
+    wordAndEt.pronunciation = wordAndEt.pronunciation.replaceAll(cleanOutputTwo, '</em>');
+    wordAndEt.short = wordAndEt.short.replaceAll(cleanOutputTwo, '</em>');
+    wordAndEt.def = wordAndEt.def.replaceAll(cleanOutputTwo, '</em>');
+
+
     display.innerHTML = `<h1>${wordAndEt.word}</h1><p>${wordAndEt.pronunciation}</p>
                          <p>1.) ${wordAndEt.short}<br>2.) ${wordAndEt.def}</p><br>
-                         <em>${wordAndEt.et}</em>`;  
+                         <p>${wordAndEt.et}</p>`;  
 
     return wordAndEt;
 
@@ -48,6 +65,7 @@ const getWord = async(input) => {
   }
 }
 
+// I am glad I wrapped it in a function, though. I may move it to another file and import it until I have time to clean it up...
 const handleKeySubmit = (e) => {
   if (e.key === 'Enter') {
     getWord();
